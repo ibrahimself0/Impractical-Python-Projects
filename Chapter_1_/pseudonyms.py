2ce224236c5f192be88864040d18244c61bb4af5
ibra <ibrahimalebrahim16@gmail.com>
2025-03-14 15:20:16 +0300

    first commit

---
+++ Chapter_1_/pseudonyms.py
@@ -0,0 +1,47 @@
+"""
+SILLY NAME GENERATOR
+C:\Impractical Python Projects\Chapter_1_
+"""
+import random
+
+
+def main():
+    """main"""
+    first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+             "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite'",
+             'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+             'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+             'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+             'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+             'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+             'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"', 'Mergatroid',
+             '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch', 'Ovaltine',
+             'Pennywhistle', 'Pitchfork Ben', 'Potato Bug', 'Pushmeet',
+             'Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+             "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+             'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+             'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+             "Winston 'Jazz Hands'", 'Worms')
+
+    last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+            'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+            'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+            'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+            'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+            'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+            'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+            'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+            'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+            'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+            'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+            'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+            'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+            'Woolysocks')
+
+    while input("Enter Q to Stop : ") != 'Q':
+        name = random.choice(first) + random.choice(last)
+        print(name)
+
+
+if __name__ == '__main__':
+    main()
